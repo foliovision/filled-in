37cc67b36cd9e54084d3a708547e89bcff124a0b
Vlado Varga <vlado.varga@foliovision.com>
2013-01-21 16:08:08 +0100

    Added protection notice. Reorganised changelog.

--- view/admin/form/form_replace.php
+++ view/admin/form/form_replace.php
@@ -1,4 +1,23 @@
-<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?>
+<?php 
+  if (!defined ('ABSPATH')) die ('No direct access allowed');
+  if( current_user_can('manage_options') ) {
+      include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); 
+      $bActive = is_plugin_active('fv-antispam/fv-antispam.php');    
+      if (!$bActive) { ?>
+        <p>
+          <small>(For better protection of this form install or activate <a href="http://wordpress.org/extend/plugins/fv-antispam/" target="_blank">FV Antispam</a>.)</small>
+        </p>
+<?php }
+      else { 
+        $aOptions = get_option('fv_antispam');
+        if (!$aOptions['protect_filledin']) { ?>
+          <p>
+            <small>(For better protection of this form check the "Protect Filled in forms" in <a href="<?php echo site_url() . '/wp-admin/options-general.php?page=fv-antispam/fv-antispam.php'?>">FV Antispam</a> options.)</small>
+          </p>  
+  <?php }
+      }
+  }
+?>
 <form method="post" action="<?php echo $action.$top ?>" <?php echo $params ?><?php echo $upload ?>>
 	<input type="hidden" name="filled_in_form" value="<?php echo $formid ?>"/>
 	<input type="hidden" name="filled_in_start" value="<?php echo $time ?>"/>
